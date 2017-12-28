4dae3b8b01121dd4b5cc7706649c7410fa8ff459
zhangyibo <600097@syswin.com>
2017-12-28 14:41:44 +0800

    # 初始化

--- analysis/show/view.py
+++ analysis/show/view.py
@@ -3,7 +3,6 @@ import os
 from analysis.utils.ip_geo import get_geo_name_by_ip
 from config import basedir
 from ..show import show_blueprint
-from flask import request
 from flask_restful import Api, Resource
 
 api = Api(show_blueprint)
@@ -11,15 +10,17 @@ api = Api(show_blueprint)
 
 class TestGeo(Resource):
     def get(self):
-        result = []
+        result = {}
         with open(os.path.join(basedir, 'analysis/show/geo.txt'), 'rb') as f:
             content_list = f.readlines()
             for ip in content_list:
                 ip = str(ip.strip(), encoding='utf-8')
                 name = get_geo_name_by_ip(ip)
-                result.append(name)
+                result[name] = ip
+
+        res = [{'name': k, 'ip': v} for k, v in result.items()]
         data = {
-            'geo_name': result
+            'res': res
         }
         return data
 
