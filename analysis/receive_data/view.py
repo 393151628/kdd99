183c6a497b426fc141c05dd4f4526ee47231ea3b
zhangyibo <600097@syswin.com>
2018-01-03 15:40:49 +0800

    # add gevent

--- analysis/receive_data/view.py
+++ analysis/receive_data/view.py
@@ -31,7 +31,7 @@ def find_first_timestamp(data, timestamp, idx=0):
 
 
 def create_queue(data):
-    queue_obj = SingletonQueue()
+    # queue_obj = SingletonQueue()
     timestamp_min = data[0].get('probe_ts')
     timestamp_max = data[-1].get('probe_ts')
     # 默认为请求来的数据是一个由时间戳升序列表
@@ -39,7 +39,7 @@ def create_queue(data):
     # 相等则表示该flow内为同一秒
     # 不等则找出不等的点进行分割
     # 认为一次请求中的连接时间戳最多相差1秒
-    diff = timestamp_min - queue_obj.timestamp_current
+    # diff = timestamp_min - queue_obj.timestamp_current
     if timestamp_min == timestamp_max:
         queue = [[], data]
         # if diff == 0:
@@ -103,14 +103,16 @@ class ReciveData(Resource):
                 dport = error_con['content'][1]
                 sip = error_con['content'][2]
                 sport = error_con['content'][3]
+                timestamp = error_con['content'][4]
                 error_type = error_con['error_type']
                 kwargs = {
-                    'dip': socket.inet_ntoa(struct.pack('I',socket.htonl(int(dip)))),
+                    'dip': socket.inet_ntoa(struct.pack('I', socket.htonl(int(dip)))),
                     'dport': str(int(dport)),
-                    'sip': socket.inet_ntoa(struct.pack('I',socket.htonl(int(sip)))),
+                    'sip': socket.inet_ntoa(struct.pack('I', socket.htonl(int(sip)))),
                     'sport': str(int(sport)),
                     'error_type': error_type[0],
-                    'error_per': str(error_type[1])
+                    'error_per': str(error_type[1]),
+                    'timestamp': str(int(timestamp)),
                 }
                 Flow.objects.create(**kwargs)
 
--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from analysis import create_app
 from config import ENV
 
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+
+monkey.patch_all()
 if __name__ == '__main__':
     app = create_app(ENV)
-    app.run(host='0.0.0.0', debug=True)
+    # app.run(host='0.0.0.0', debug=True)
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
