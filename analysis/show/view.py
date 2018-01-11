b7008c9e055c3cd3aa0f015db7fd5fc8ba51ee12
liuyong <liuyong@syswin.com>
2018-01-11 11:16:55 +0800

    与前端对接1.0.5 模型策略优化

--- analysis/show/view.py
+++ analysis/show/view.py
@@ -9,7 +9,6 @@ from config import basedir
 from ..show import show_blueprint
 from flask_restful import Api, Resource
 import pandas as pd
-import json
 
 from analysis.models import Flow
 
@@ -42,51 +41,35 @@ class AbnormalEvent(Resource):
     def get(self):
         logging.info(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())) + ' : get abnormal event.')
         time_strip = int(time.mktime(time.localtime()))
+        count, att, eventList = [], [], []
         his_num = Flow.objects.filter(timestamp__gt=str(time_strip-time_strip%86400),timestamp__lte=str(time_strip-60)).count()
-        logging.info('his_num: {}'.format(his_num))
-        event_obj = Flow.objects.filter(timestamp__gt=str(time_strip-556400),timestamp__lte=str(time_strip)).limit(100)
+        # logging.info('his_num: {}'.format(his_num))
+        event_obj = Flow.objects.filter(timestamp__gt=str(time_strip-60),timestamp__lte=str(time_strip)).limit(100)
         event_all = [[event.id, event.dip, event.dport, event.sip, event.sport, event.error_type, event.error_per, event.timestamp] for event in event_obj]
         event_df = pd.DataFrame(event_all, columns=['id', 'dip', 'dport', 'sip', 'sport', 'error_type', 'error_per', 'timestamp'])
 
         event_count = event_df.groupby('timestamp')['dip'].count()
-        # event_count = event_count.reset_index()
-        # event_count = event_count.rename(columns={'timestamp': 'time', 'dip': 'value'})
-        #零填充
-        # his_count = pd.DataFrame([[int(time_strip) + i, 0] for i in range(60)], columns=['time', 'value'])
-        # event_count = pd.concat([event_count, his_count]).astype(int)
-        # count = list(event_count['value'].cumsum()+his_num)[-60:]
-        #
-        # event_count['time'] = event_count['time'].apply(lambda x: time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(x)))
-        # att = [{"name": row['time'], "value": [row['time'], row['value']]} for row in
-        #        event_count.to_dict(orient='records')[-60:]]
 
-        # count = []
-        # att = []
-        # event_count = event_count.to_dict()
-        # time_strip = int(event_all[-1][-1])
-        # for i in range(-59, 1):
-        #     time_str = str(time_strip + i)
-        #     if event_count.get(time_str):
-        #         his_num = his_num + event_count[time_str]
-        #         count.append(his_num)
-        #         strip_time = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(int(time_str)))
-        #         att.append({"name": strip_time, "value": [strip_time, event_count[time_str]]})
-        #     else:
-        #         count.append(his_num)
-        #         strip_time = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(int(time_str)))
-        #         att.append({"name": strip_time, "value": [strip_time, 0]})
+        #实时数据
+        beg_time = int(time_strip)
+        # beg_time = int(event_count.index[-1])
+        cur_count = event_count.reindex(index=[str(beg_time + i) for i in range(-59,1)], fill_value=0)
 
-        #零填充测试
-        count = []
-        att = []
-        # event_count = event_count.to_dict()
-        # time_strip = int(event_all[-1][-1])
         for i in range(-59,1):
-            val = randint(1,5)
+            val = int(cur_count[i])
             his_num = his_num+ val
             count.append(his_num)
             strip_time = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time_strip+i))
             att.append({"name": strip_time, "value": [strip_time , val]})
+
+
+        #测试数据
+        # for i in range(-59,1):
+        #     val = randint(1,5)
+        #     his_num = his_num+ val
+        #     count.append(his_num)
+        #     strip_time = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time_strip+i))
+        #     att.append({"name": strip_time, "value": [strip_time , val]})
 
 
         event_len = len(event_all)
@@ -94,7 +77,6 @@ class AbnormalEvent(Resource):
             return int(num / event_len * 100)
 
         level_dict = event_df.groupby('dip')['timestamp'].count().astype(int).to_dict()
-        eventList = []
         level_scr = 0
         if len(event_count):
             for row in event_all[-10:]:
@@ -129,8 +111,6 @@ class AbnormalEvent(Resource):
                               "EventProbability": row[6],
                               "EventDate": time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(int(row[7])))} )
 
-        else: eventList = []
-
         res = {'count' : count,
                'att' : att,
                'eventList' : eventList,
@@ -138,7 +118,6 @@ class AbnormalEvent(Resource):
                }
 
         return res
-
 
 api.add_resource(TestGeo, '/testgeo')
 api.add_resource(MyTest, '/mytest')
