98ba7e274fe5aca137b45b5513909775b461fe77
zhangyibo <600097@syswin.com>
2018-01-18 17:16:00 +0800

    # fix show

--- analysis/show/view.py
+++ analysis/show/view.py
@@ -39,34 +39,40 @@ class MyTest(Resource):
         logging.info('******************')
         return 'ddddddddddd'
 
+
 class AbnormalEvent(Resource):
     def get(self):
         logging.info(time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time.time())) + ' : get abnormal event.')
         time_strip = int(time.mktime(time.localtime()))
-        count, att, eventList = [], [], []
-        his_num = Flow.objects.filter(timestamp__gt=str(time_strip-time_strip%86400),timestamp__lte=str(time_strip-60)).count()
+        count = att = eventList = []
+        last_minute = time_strip - 60
+        his_num = Flow.objects.filter(createdtime__gt=str(time_strip - time_strip % 86400),
+                                      createdtime__lte=str(last_minute)).count()
         # logging.info('his_num: {}'.format(his_num))
-        a = Flow.objects.filter(created_time__exists=True)
-        event_obj = a.filter(created_time__gt=str(time_strip-60),created_time__lte=str(time_strip))
-        event_all = [[event.id, event.dip, event.dport, event.sip, event.sport, event.error_type, event.error_per, event.timestamp] for event in event_obj]
-        event_df = pd.DataFrame(event_all, columns=['id', 'dip', 'dport', 'sip', 'sport', 'error_type', 'error_per', 'timestamp'])
+        event_obj = Flow.objects.filter(createdtime__gt=str(last_minute), createdtime__lte=str(time_strip))
+        event_all = [
+            [event.id, event.dip, event.dport, event.sip, event.sport, event.error_type, event.error_per,
+             event.timestamp]
+            for event in event_obj
+        ]
+        event_df = pd.DataFrame(event_all,
+                                columns=['id', 'dip', 'dport', 'sip', 'sport', 'error_type', 'error_per', 'timestamp'])
 
         event_count = event_df.groupby('timestamp')['dip'].count()
 
-        #实时数据
+        # 实时数据
         beg_time = int(event_count.index[-1])
         # beg_time = int(event_count.index[-1])
-        cur_count = event_count.reindex(index=[str(beg_time + i) for i in range(-59,1)], fill_value=0)
+        cur_count = event_count.reindex(index=[str(beg_time + i) for i in range(-59, 1)], fill_value=0)
         cst_tz = timezone('Asia/Shanghai')
-        for i in range(-59,1):
+        for i in range(-59, 1):
             val = int(cur_count[i])
-            his_num = his_num+ val
+            his_num = his_num + val
             count.append(his_num)
-            strip_time = datetime.fromtimestamp(beg_time+i).astimezone(cst_tz).strftime('%Y-%m-%d %H:%M:%S')
-            att.append({"name": strip_time, "value": [strip_time , val]})
+            strip_time = datetime.fromtimestamp(beg_time + i).astimezone(cst_tz).strftime('%Y-%m-%d %H:%M:%S')
+            att.append({"name": strip_time, "value": [strip_time, val]})
 
-
-        #测试数据
+        # 测试数据
         # for i in range(-59,1):
         #     val = randint(1,5)
         #     his_num = his_num+ val
@@ -74,8 +80,8 @@ class AbnormalEvent(Resource):
         #     strip_time = time.strftime('%Y/%m/%d %H:%M:%S', time.localtime(time_strip+i))
         #     att.append({"name": strip_time, "value": [strip_time , val]})
 
-
         event_len = len(event_all)
+
         def _event_level(num):
             return int(num / event_len * 100)
 
@@ -86,41 +92,44 @@ class AbnormalEvent(Resource):
                 scr_level = _event_level(level_dict[row[1]])
                 level_scr += scr_level
                 eventList.append({"Tip": row[1],
-                              "Tport": row[2],
-                              "Tname": get_geo_name_by_ip(row[1]),
-                              "Sname": get_geo_name_by_ip(row[3]),
-                              "Sip": row[3],
-                              "Sport": row[4],
-                              "type": lan_ip(row[1]),
-                              "EventName": str(row[0]),
-                              "EventType": row[5],
-                              "EventDes": '{}({}) -> {}({})'.format(row[3], row[4],row[1], row[2]),
-                              "EventLevel": int(scr_level),
-                              "EventProbability": row[6],
-                              "EventDate": datetime.fromtimestamp(int(row[7])).astimezone(cst_tz).strftime('%Y-%m-%d %H:%M:%S')})
-            if event_len>10:
+                                  "Tport": row[2],
+                                  "Tname": get_geo_name_by_ip(row[1]),
+                                  "Sname": get_geo_name_by_ip(row[3]),
+                                  "Sip": row[3],
+                                  "Sport": row[4],
+                                  "type": lan_ip(row[1]),
+                                  "EventName": str(row[0]),
+                                  "EventType": row[5],
+                                  "EventDes": '{}({}) -> {}({})'.format(row[3], row[4], row[1], row[2]),
+                                  "EventLevel": int(scr_level),
+                                  "EventProbability": row[6],
+                                  "EventDate": datetime.fromtimestamp(int(row[7])).astimezone(cst_tz).strftime(
+                                      '%Y-%m-%d %H:%M:%S')})
+            if event_len > 10:
                 for row in event_all[-100:-10]:
                     scr_level = _event_level(level_dict[row[1]])
                     level_scr += scr_level
                     eventList.append({"Tip": row[1],
-                              "Tport": row[2],
-                              "Sip": row[3],
-                              "Sport": row[4],
-                              "type": lan_ip(row[1]),
-                              "EventName": str(row[0]),
-                              "EventType": row[5],
-                              "EventDes": '{}({}) -> {}({})'.format(row[3], row[4], row[1], row[2]),
-                              "EventLevel": int(scr_level),
-                              "EventProbability": row[6],
-                              "EventDate": datetime.fromtimestamp(int(row[7])).astimezone(cst_tz).strftime('%Y-%m-%d %H:%M:%S') })
+                                      "Tport": row[2],
+                                      "Sip": row[3],
+                                      "Sport": row[4],
+                                      "type": lan_ip(row[1]),
+                                      "EventName": str(row[0]),
+                                      "EventType": row[5],
+                                      "EventDes": '{}({}) -> {}({})'.format(row[3], row[4], row[1], row[2]),
+                                      "EventLevel": int(scr_level),
+                                      "EventProbability": row[6],
+                                      "EventDate": datetime.fromtimestamp(int(row[7])).astimezone(cst_tz).strftime(
+                                          '%Y-%m-%d %H:%M:%S')})
 
-        res = {'count' : count,
-               'att' : att,
-               'eventList' : eventList,
-               'level' : int(level_scr/(len(event_all[-100:])+5))
+        res = {'count': count,
+               'att': att,
+               'eventList': eventList,
+               'level': int(level_scr / (len(event_all[-100:]) + 5))
                }
 
         return res
+
 
 api.add_resource(TestGeo, '/testgeo')
 api.add_resource(MyTest, '/mytest')
